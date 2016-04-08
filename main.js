fd481b0564c0e4598861a04c08cb3451e3ff7459
cigar <1255828611@qq.com>
2016-04-08 17:13:35 +0800

    change

--- main.js
+++ main.js
@@ -6,12 +6,11 @@
  *{name:"ease-in",property:"all",duration:0.5,bezier:{x1:0.5,y1:0.5,x2:0.5,y2:0.5},delay:0.5}
  *库:lib当前库，lib是一个数组
  *属性：
- *  当前画板属性(p1,p2)
+ *  DOM对象
+ *  当前画板属性(p1,p2坐标)
  *  当前区域属性
  *  展示中属性
- *
- *
- *方法
+ *方法：
  *  鼠标位置获取
  *  值转换（将鼠标位置转换为transition值，和将transition值转换为鼠标位置）
  *  监听控制点，并操作
@@ -27,19 +26,25 @@ function byId(id){
 * 定义命名空间transitionMaker
 */
 var transitionMaker={
-    p1:{x:0.25,y:0.1},//p1控制点坐标
-    p2:{x:0.25,y:1},//p2控制点坐标
+    canvas:null,
+    context2d:null,
+    property:null,//transition目标属性
+    duration:null,//transition时间
+    bezier:null,//贝塞尔曲线
+    delay:null,//transition延时
+    p1:{x:0.25,y:0.1},//p1控制点 坐标
+    p2:{x:0.25,y:1},//p2控制点 坐标
     mousePosition:{x:1,y:1}//鼠标位置
-    property:null,
 };
+transitionMaker.propertyLib=[];
 transitionMaker.lib=[];
 /*
 *监听鼠标移动
 */
-transitionMaker.getMousePosition=function(element){
+transitionMaker.getMousePosition=function(elementParent,element){
     var mouse={x:0,y:0};
-    element.addEventListener("mousemove",function(event){
-        var x,y;
+    elementParent.addEventListener("mousemove",function(event){
+        var x,y,x0,y0;
         if(event.pageX||event.pageY){
             x=event.pageX;
             y=event.pageY;
@@ -50,7 +55,7 @@ transitionMaker.getMousePosition=function(element){
         x-=element.offsetLeft;
         y-=element.offsetTop;
         mouse.x=x;
-        mouse.y=y-60;
+        mouse.y=y;
     },false);
     return mouse;
 };
@@ -65,9 +70,11 @@ transitionMaker.pointToMouse=function(px,py){
 };
 /*
 * 获取控制点
+*   传参（控制点，控制点名）
 */
 transitionMaker.getController=function(element,name){
     var control=null;
+    /*获取坐标*/
     /*保证控制点在canvas画布内*/
     function addMoveListener(){
         var x,y;
@@ -92,6 +99,7 @@ transitionMaker.getController=function(element,name){
             y=transitionMaker.mousePosition.y;
         }
         transitionMaker[name]=transitionMaker.mouseToPoint(x,y);
+        transitionMaker.changeData();
         console.log(name+":"+transitionMaker[name].x+","+transitionMaker[name].y);
     }
     /*
@@ -108,22 +116,28 @@ transitionMaker.getController=function(element,name){
 /*
  * 改变头部transition的值
  */
-function changeBezier(p1,p2){
-
+transitionMaker.changeData=function (){
+    transitionMaker.bezier.innerHTML=transitionMaker.p1.x+","+transitionMaker.p1.y+","+transitionMaker.p2.x+","+transitionMaker.p2.y;
 }
+/*改变坐标值*/
+transitionMaker.changeXY=function(){
 
-
+};
 /*
  *初始化
  *   初始变量
  *   画布
 */
-transitionMaker.init=function(){
-    return 1;
+transitionMaker.init=function(property,duration,bezier,delay,canvas){
+    transitionMaker.property=byId("property");
 };
 window.onload=function(){
-    transitionMaker.mousePosition=transitionMaker.getMousePosition(document.body);
-    document.onmousemove=function(){byId("showContainer").innerHTML="pos:"+ transitionMaker.mousePosition.x+","+ transitionMaker.mousePosition.y;};
+    transitionMaker.property=byId("property");
+    transitionMaker.duration=byId("duration");
+    transitionMaker.bezier=byId("bezier");
+    transitionMaker.delay=byId("delay");
+    transitionMaker.mousePosition=transitionMaker.getMousePosition(document.body,byId("canvasContainer1"));
     transitionMaker.getController(byId("p1"),"p1");
     transitionMaker.getController(byId("p2"),"p2");
+    transitionMaker.getController(byId("c1"),"c1");
 };
